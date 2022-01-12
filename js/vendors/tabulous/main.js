528c388cb9dfce2a3d5d74b16f5d01b73027a554
Ogechukwu Nwike <oge.nwike@gmail.com>
2022-01-12 20:42:38 +0100

    start

---
+++ js/vendors/tabulous/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function($) {
+    
+
+    $('#tabs1').tabulous({
+    	effect: 'slideLeft'
+    });
+
+     $('#tabs2').tabulous({
+    	effect: 'slideLeft'
+    });
+
+     $('#tabs3').tabulous({
+    	effect: 'slideLeft'
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
